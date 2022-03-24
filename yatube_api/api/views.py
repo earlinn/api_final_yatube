4faaa727f2d49d0b48d17ba0f59fba4bf1a779e5
Galina Volkova <earlinndrow@gmail.com>
2022-03-24 14:18:06 +0300

    Prevent users from editing and deleting other users' posts

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,12 +2,14 @@ from rest_framework import viewsets
 
 from posts.models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
 
 class PostViewSet(viewsets.ModelViewSet):
     """Вьюсет для чтения, создания, изменения и удаления постов."""
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
